33d58cd3279c2a8a53a772001454be134abe66f4
tiger <nazrulislamrakib1813030@gmail.com>
2025-05-20 00:26:05 +0600

    "Import libraries and assign stopwords"

---
+++ chat_summarizer.py
@@ -0,0 +1,10 @@
+import os
+import string
+import re 
+from collections import Counter
+
+stopwords = set([
+    'the', 'is', 'and', 'a', 'an', 'can', 'you', 'for', 'to', 'of', 'in', 'it', 'its', 'this', 'that', 'on', 'with'
+])
+def chat_log_parsing(file_path):
+    pass
