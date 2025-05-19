337a1f7a0914d3959aea35eabd2fe704febc2d81
tiger <nazrulislamrakib1813030@gmail.com>
2025-05-20 00:53:05 +0600

    "Open txt file and read all the lines"

--- chat_summarizer.py
+++ chat_summarizer.py
@@ -7,4 +7,10 @@ stopwords = set([
     'the', 'is', 'and', 'a', 'an', 'can', 'you', 'for', 'to', 'of', 'in', 'it', 'its', 'this', 'that', 'on', 'with'
 ])
 def chat_log_parsing(file_path):
-    pass
+    with open(file_path,'r') as file:
+        lines= file.readlines()
+    for line in lines:
+        print(line.strip())
+
+file="example.txt"
+chat_log_parsing(file)
